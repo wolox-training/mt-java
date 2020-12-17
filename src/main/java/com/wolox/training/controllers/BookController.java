beb6a03ec5a8bca355b910e477cc5e477803c2e4
Matias Torsello <matiastorsello@gmail.com>
2020-12-17 10:49:46 -0300

    Modifying Book Controller RequestMapping

--- src/main/java/com/wolox/training/controllers/BookController.java
+++ src/main/java/com/wolox/training/controllers/BookController.java
@@ -15,10 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
+@RequestMapping("/api/books")
 public class BookController {
 
     @Autowired

85f18597801443b6384aade1d650c5946fcae72f
Matias Torsello <matiastorsello@gmail.com>
2020-12-18 10:56:52 -0300

    Javadoc Methods comments

--- src/main/java/com/wolox/training/services/BookService.java
+++ src/main/java/com/wolox/training/services/BookService.java
@@ -14,23 +14,47 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class BookService {
 
-    private static final String BOOK = "book";
+    public static final String BOOK = "Book";
 
     @Autowired
     private BookRepository bookRepository;
 
+    /**
+     * This method returns a list of {@link Book}
+     *
+     * @return the list of {@link Book} persisted on db
+     */
     public List<Book> findAll() {
         return bookRepository.findAll();
     }
 
+    /**
+     * This method returns a {@link Book} wrote by an specific Author
+     *
+     * @param bookAuthor: name of the book's author
+     * @return a {@link Book} wrote by a bookAuthor
+     */
     public Optional<Book> findByAuthor(String bookAuthor) {
         return bookRepository.findByAuthor(bookAuthor);
     }
 
+    /**
+     * This method returns a {@link Book} by id
+     *
+     * @param id: book's id
+     * @return a {@link Book} by id
+     */
     public Book findOne(Long id) {
         return bookRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(BOOK));
     }
 
+
+    /**
+     * This method creates a {@link Book}
+     *
+     * @param bookDto: Data Transfer Object of {@link Book}
+     * @return the {@link Book} created
+     */
     @Transactional
     public Book create(BookDTO bookDto) {
         Book book = new Book();
@@ -38,6 +62,11 @@ public class BookService {
         return bookRepository.save(book);
     }
 
+    /**
+     * This method deletes a {@link Book}
+     *
+     * @param id: book's id
+     */
     @Transactional
     public void delete(Long id) {
         bookRepository.findById(id)
@@ -45,6 +74,13 @@ public class BookService {
         bookRepository.deleteById(id);
     }
 
+    /**
+     * This method updates an specific {@link Book}
+     *
+     * @param bookDto: Data Transfer Object of {@link Book} with the new values
+     * @param id:      book's id
+     * @return the {@link Book} updated
+     */
     @Transactional
     public Book updateBook(BookDTO bookDto, Long id) {
         Book book = bookRepository.findById(id)

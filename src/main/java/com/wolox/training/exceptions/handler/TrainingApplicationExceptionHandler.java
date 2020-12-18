77f4cf3bfb7d35c14f428352e1ea747a4abe1269
Matias Torsello <matiastorsello@gmail.com>
2020-12-18 14:32:21 -0300

    Feature User Model and Exception

--- src/main/java/com/wolox/training/exceptions/handler/TrainingApplicationExceptionHandler.java
+++ src/main/java/com/wolox/training/exceptions/handler/TrainingApplicationExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.wolox.training.exceptions.handler;
 
+import com.wolox.training.exceptions.BookAlreadyOwnedException;
 import com.wolox.training.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,15 +11,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class TrainingApplicationExceptionHandler {
 
-    private static final String NOT_FOUND = "not found";
-
     @ExceptionHandler({ObjectNotFoundException.class})
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ResponseBody
     public GenericError objectNotFound(ObjectNotFoundException ex) {
-        String message = new StringBuilder(ex.getObjectName()).append(" ").append(NOT_FOUND)
+        ErrorsEnum error = ErrorsEnum.OBJECT_NOT_FOUND;
+        String message = new StringBuilder(ex.getObjectName()).append(" ").append(error.getMessage())
                 .toString();
-        return new GenericError("001", message);
+        return new GenericError(error.getCode(), message);
+    }
+
+    @ExceptionHandler({BookAlreadyOwnedException.class})
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseBody
+    public GenericError bookAlreadyOwned(BookAlreadyOwnedException ex) {
+        ErrorsEnum error = ErrorsEnum.BOOK_ALREADY_OWNED;
+        return new GenericError(error.getCode(), error.getMessage());
     }
 
 }
--- src/main/java/com/wolox/training/models/Book.java
+++ src/main/java/com/wolox/training/models/Book.java
@@ -1,10 +1,13 @@
 package com.wolox.training.models;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -39,6 +42,10 @@ public class Book {
 
     @Column(nullable = false, unique = true)
     private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name="books")
+    private Users user;
 
     public Book() {
 
@@ -120,4 +127,28 @@ public class Book {
         this.isbn = isbn;
     }
 
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return isbn.equals(book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
 }

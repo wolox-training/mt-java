dcfed5ecfa5a487f09fcc777464ae77920568ae1
Matias Torsello <matiastorsello@gmail.com>
2020-12-18 14:42:38 -0300

    Many to Many relationship (Book and User)

--- src/main/java/com/wolox/training/models/Book.java
+++ src/main/java/com/wolox/training/models/Book.java
@@ -1,12 +1,17 @@
 package com.wolox.training.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
@@ -43,9 +48,12 @@ public class Book {
     @Column(nullable = false, unique = true)
     private String isbn;
 
-    @ManyToOne
-    @JoinColumn(name="books")
-    private Users user;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "books_users",
+            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id",
+                    referencedColumnName = "id"))
+    private List<Users> users = new ArrayList<>();
 
     public Book() {
 
@@ -127,12 +135,12 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public Users getUser() {
-        return user;
+    public List<Users> getUsers() {
+        return users;
     }
 
-    public void setUser(Users user) {
-        this.user = user;
+    public void setUsers(List<Users> users) {
+        this.users = users;
     }
 
     @Override
--- src/main/java/com/wolox/training/models/Users.java
+++ src/main/java/com/wolox/training/models/Users.java
@@ -9,7 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -28,8 +28,7 @@ public class Users {
     @NotNull
     private LocalDate birthdate;
 
-    @NotNull
-    @OneToMany(mappedBy = "users")
+    @ManyToMany(mappedBy = "users")
     private List<Book> books = new ArrayList<>();
 
     public Users() {

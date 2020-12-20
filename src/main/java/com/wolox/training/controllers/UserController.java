8b4cebab6d58708d364b688f84d208e857be8737
Matias Torsello <matiastorsello@gmail.com>
2020-12-20 15:23:27 -0300

    specifying name of pathVariable

--- src/main/java/com/wolox/training/controllers/UserController.java
+++ src/main/java/com/wolox/training/controllers/UserController.java
@@ -61,13 +61,13 @@ public class UserController {
     }
 
     @PostMapping("/{id}/add")
-    public ResponseEntity<Users> add(@PathVariable Long userId, @RequestParam Long bookId) {
+    public ResponseEntity<Users> add(@PathVariable(name = "id") Long userId, @RequestParam Long bookId) {
         Users user = userService.addBookToAUser(userId, bookId);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @PostMapping("/{id}/remove")
-    public ResponseEntity<Users> create(@PathVariable Long userId, @RequestParam Long bookId) {
+    public ResponseEntity<Users> create(@PathVariable(name = "id") Long userId, @RequestParam Long bookId) {
         Users user = userService.removeBookToAUser(userId, bookId);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }

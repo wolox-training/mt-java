34a9161e3c53e4dd2fafe59189016da83655b050
Matias Torsello <matiastorsello@gmail.com>
2020-12-29 13:47:53 -0300

    Renaming Spanish variable +Using string format to reuse api url constant

--- src/main/java/com/wolox/training/services/OpenLibraryService.java
+++ src/main/java/com/wolox/training/services/OpenLibraryService.java
@@ -16,25 +16,25 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class OpenLibraryService {
 
-    private static final String BASE_URL = "https://openlibrary.org/api/books";
+    private static final String BASE_URL = "https://openlibrary.org/api/books?bibkeys=%s&format=json&jscmd=data";
 
     public Optional<BookInfoDTO> bookInfo(String isbn) {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
         objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        RestTemplate restTemplate = new RestTemplate();
 
-        String url = new StringBuilder(BASE_URL).append("?bibkeys=ISBN:").append(isbn)
-                .append("&format=json&jscmd=data").toString();
+        RestTemplate restTemplate = new RestTemplate();
+        String queryParam = "ISBN:" + isbn;
+        String url = String.format(BASE_URL, queryParam);
         BookInfoDTO bookInfo = new BookInfoDTO();
 
         try {
             JsonNode bookInfoJsonNode = objectMapper
                     .readTree(restTemplate.getForObject(url, String.class));
-            HashMap<String, Object> mapa = objectMapper
-                    .treeToValue(bookInfoJsonNode.get("ISBN:" + isbn), HashMap.class);
-            bookInfo = objectMapper.convertValue(mapa, BookInfoDTO.class);
+            HashMap<String, Object> map = objectMapper
+                    .treeToValue(bookInfoJsonNode.get(queryParam), HashMap.class);
+            bookInfo = objectMapper.convertValue(map, BookInfoDTO.class);
 
             if (bookInfo != null) {
                 bookInfo.setIsbn(isbn);
